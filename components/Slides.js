71d1501ec06dba964c59d90d932cfe65ff70dd71
William T <william.tam18@gmail.com>
2017-11-24 08:40:42 +0800

    helper method in Slides.js to add button to last slide of welcomeScreen

--- components/Slides.js
+++ components/Slides.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
 import { View, Text, ScrollView, Dimensions } from 'react-native';
+import { Button } from 'react-native-elements'
 
-const screenWidth = Dimensions.get('window').width
+const screenWidth = Dimensions.get('window').width;
 
 // just defining horizontal sets it as true, no need for horizontal={true}
 class Slides extends Component {
+    renderLastSlide(index) {
+        if (index === this.props.data.length - 1) {
+            return (
+                <Button style={styles.buttonStyle} title="Get Dat Paper" raised />
+            )
+        }
+    }
+
     renderSlides() {
-        return this.props.data.map((slide) => {
+        return this.props.data.map((slide, index) => {
             return (
                 <View key={slide.text} style={[styles.slideStyle, {backgroundColor: slide.color}]}>
                     <Text style={styles.textStyle}>
                         {slide.text}
                     </Text>
+                    { this.renderLastSlide(index) }
                 </View>
             )
         })
@@ -34,7 +44,11 @@ const styles = {
         width: screenWidth
     },
     textStyle: {
-        fontSize: 30
+        fontSize: 30,
+        color: 'white'
+    },
+    buttonStyle: {
+        marginTop: 5
     }
 };
 
