135a58de2a24a9a3fd5960d6b835cab178bbb693
William T <william.tam18@gmail.com>
2017-11-26 02:01:42 +0800

    edit package.json, renable asyncstorage remove

--- screens/AuthScreen.js
+++ screens/AuthScreen.js
@@ -6,7 +6,7 @@ import * as actions from '../actions';
 class AuthScreen extends Component {
     componentDidMount() {
         this.props.facebookLogin();
-        // AsyncStorage.removeItem('fb_token')
+        AsyncStorage.removeItem('fb_token')
     }
 
     render () {
