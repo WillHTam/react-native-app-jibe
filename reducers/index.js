0c0a8024a9a7a1862cbef6934876316251a933c5
William T <william.tam18@gmail.com>
2017-11-27 03:34:19 +0800

    successful redirect to map screen on successful facebook login

--- reducers/index.js
+++ reducers/index.js
@@ -1,6 +1,12 @@
 import { combineReducers } from 'redux';
+import auth from './auth_reducer'
 
 // In Redux, must have at least one reducer which cannot return undefined. 
 export default combineReducers({
-    auth: () => { return {} }
+    // es6 allows this instead of auth: auth
+    auth
 });
+
+// auth contains a property called token
+    // if token = false, go into facebook login flow
+    // if exists, continue onto map screen 
--- screens/AuthScreen.js
+++ screens/AuthScreen.js
@@ -6,19 +6,36 @@ import * as actions from '../actions';
 class AuthScreen extends Component {
     componentDidMount() {
         this.props.facebookLogin();
-        AsyncStorage.removeItem('fb_token')
+        // line below will clear saved token
+        // AsyncStorage.removeItem('fb_token');
+        // this onAuthComplete is unlikely to be called because it
+            // is waiting for facebookLogin() to be complete
+        this.onAuthComplete(this.props);
+    }
+
+    // Called whenever a component is about to rerender with props
+    componentWillReceiveProps(nextProps) {
+        // activated on successful login
+        this.onAuthComplete(nextProps);
+    }
+
+    // this function will check for a token, and redirect appropriately if it exists
+    onAuthComplete(props) {
+        if (props.token) {
+            this.props.navigation.navigate('map');
+        }
     }
 
     render () {
         return(
-            <View>
-                <Text>AuthScreen</Text>
-                <Text>AuthScreen</Text>
-                <Text>AuthScreen</Text>
-            </View>
+            <View />
         );
     }
 };
 
+function mapStateToProps({ auth }) {
+    return { token: auth.token};
+}
+
 // first argument is map state to props, and the second is action creators
-export default connect(null, actions)(AuthScreen);
+export default connect(mapStateToProps, actions)(AuthScreen);
