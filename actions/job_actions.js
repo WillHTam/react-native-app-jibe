cc3a754466076f6b8e974c67b4bffd21632ab333
William T <william.tam18@gmail.com>
2017-11-28 18:51:01 +0800

    in action creator for fetchin jobs, convert latlongto zip, use qs to format url

---
+++ actions/job_actions.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import reverseGeocode from 'latlng-to-zip';
+import qs from 'qs';
+
+import {
+    FETCH_JOBS
+} from './types';
+
+const JOB_ROOT_URL = 'http://api.indeed.com/ads/apisearch?';
+// radius in miles
+const JOB_QUERY_PARAMS = {
+  publisher: '4201738803816157',
+  format: 'json',
+  v: '2',
+  latlong: 1,
+  radius: 10,
+  q: 'data scientist'
+};
+
+// this helper function will take the zip and add it to the url
+const buildJobsUrl = (zip) => {
+    const query = qs.stringify({...JOB_QUERY_PARAMS, l:zip});
+    // add the query to the end of the root url
+    return `${JOB_ROOT_URL}${query}`;
+};
+
+export const fetchJobs = (region) => async (dispatch) => {
+    try {
+        let zip = await reverseGeocode(region);
+
+    } catch(e) {
+        console.error(e);
+    }
+};
+
+// Using this action is asynchronous (fetching data), so use async-await
