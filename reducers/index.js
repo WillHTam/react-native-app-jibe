48ad207bed17a518ae23148e235086ec0e368b1e
William T <william.tam18@gmail.com>
2017-11-28 19:41:38 +0800

    auto redirect after response from Indeed

--- reducers/index.js
+++ reducers/index.js
@@ -1,10 +1,11 @@
 import { combineReducers } from 'redux';
-import auth from './auth_reducer'
+import auth from './auth_reducer';
+import jobs from './jobs_reducer';
 
 // In Redux, must have at least one reducer which cannot return undefined. 
 export default combineReducers({
-    // es6 allows this instead of auth: auth
-    auth
+    // es6 allows 'auth' instead of 'auth: auth'
+    auth, jobs
 });
 
 // auth contains a property called token
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import { View, Text, ActivityIndicator } from "react-native";
+import { Button } from 'react-native-elements'
 import { MapView } from 'expo';
+import { connect } from 'react-redux';
+
+import * as actions from '../actions';
 
 class MapScreen extends Component {
   state = {
@@ -22,6 +26,12 @@ class MapScreen extends Component {
     this.setState({ region })
   }
 
+  onButtonPress = () => {
+    this.props.fetchJobs(this.state.region, () => {
+      this.props.navigation.navigate('deck')
+    });
+  }
+
   render() {
     if (!this.state.mapLoaded) {
       return (
@@ -37,12 +47,30 @@ class MapScreen extends Component {
           style={{flex:1}}
           onRegionChangeComplete={this.onRegionChangeComplete} 
         />
+        <View style={styles.buttonContainer}>
+          <Button 
+            large
+            title="Search Here"
+            backgroundColor="#009688"
+            icon={{ name: 'search' }}
+            onPress={this.onButtonPress}
+          />
+        </View>
       </View>
     );
   }
 }
 
-export default MapScreen;
+const styles = {
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 20,
+    left: 0,
+    right: 0
+  }
+}
+
+export default connect(null, actions)(MapScreen);
 
 // Pass in 'region' prop to define where the map should start
   // long/lat are the starting position, deltas are for the zoom level
