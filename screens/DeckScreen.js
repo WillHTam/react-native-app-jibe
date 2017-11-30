b0e13699f359dbad364fc36f5c34d0406287bdc5
William T <william.tam18@gmail.com>
2017-12-01 06:36:55 +0800

    change icon on deck screen

--- screens/DeckScreen.js
+++ screens/DeckScreen.js
@@ -12,7 +12,7 @@ class DeckScreen extends Component {
   static navigationOptions = {
     title: 'Jobs',
     tabBarIcon: ({tintColor}) => {
-      return <Icon name='description' size={30} color={tintColor} />
+      return <Icon name='hand-spock-o' size={30} color={tintColor} />
     }
   }
 
